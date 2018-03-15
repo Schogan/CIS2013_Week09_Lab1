5c416f0b43b2ae30952aa1c4d1e04f11c9ebd316
Schogan <navy15@gmail.com>
2018-03-14 20:11:54 -0500

    first and last names

---
+++ CIS2013_Week09_Lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	char name[6] = "Logan";
+	char last[13] = "Schanbacher";
+	
+	cout << "Firstname: " << name << " Lastname: " << last << endl;
+	
+}
