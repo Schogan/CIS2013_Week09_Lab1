4bdb3e1feca8ab76a23c1480f83c4d45033534c4
Schogan <navy15@gmail.com>
2018-03-14 20:16:04 -0500

    strcpy

--- CIS2013_Week09_Lab1.cpp
+++ CIS2013_Week09_Lab1.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
 int main(){
 	char name[6] = "Logan";
 	char last[13] = "Schanbacher";
+	char copy[15];
 	
-	cout << "Firstname: " << name << " Lastname: " << last << endl;
+	strcpy(copy, last);
+	
+	
+	cout << "Firstname: " << name << " Lastname: " << copy << endl;
 	
 }
